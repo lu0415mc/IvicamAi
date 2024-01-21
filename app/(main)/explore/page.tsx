7d04bca42ba1721c2e7a092b32f515210afab4dd
slyfox0415 <slyfox0415@outlook.com>
2024-01-22 01:39:13 +0530

    fourthcommit

--- app/(main)/explore/page.tsx
+++ app/(main)/explore/page.tsx
@@ -1,10 +1,12 @@
 import React from "react";
 import MainNavbar from "../_components/MainNavbar";
 import Footer from "../_components/Footer";
+import ExploreCard from "../_components/ExploreCard";
 
 const ExplorePage = () => {
   return <>
   <MainNavbar/>
+  <ExploreCard/>
   <Footer/>
   </>;
 };
